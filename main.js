59c458cf5cbc06f3319dc4231440d133f45d288f
Nicholas Klunder <nklunder@gmail.com>
2014-06-16 20:01:35 -0700

    first commit - testing

--- main.js
+++ main.js
@@ -2,3 +2,28 @@
 document.getElementById("nojs").style.display = "none";
 
 // Add game logic
+
+//Sucky and incomplete code. Will rewrite / complete in the future. Just learning how to work with Git. 
+function getPlayerChoice() {
+
+
+	return playerChoice;
+}
+
+function getComputerChoice() {
+	var computerChoice = Math.random();
+
+	if (computerChoice < 0.34) {
+		computerChoice = "rat";
+	} else if (computerChoice < 0.67) {
+		computerChoice = "pig";
+	} else {
+		computerChoice = "squid";
+	}
+
+	return computerChoice;
+}
+
+function compareAnimals(userChoice, computerChoice) {
+
+}
