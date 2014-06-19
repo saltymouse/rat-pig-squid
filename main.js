87cdfeddc36d0f03018d8d1b2e5eb1195dc356d1
salty mouse <saltymouse@github.com>
2014-06-19 13:23:16 +0800

    do away with alertbox results; now displayed inline with cpu image; only works with pig and squid now

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // Remove JS warning for JS users
+document.getElementById("nojs").style.display = "none";
 
-	document.getElementById("nojs").style.display = "none";
+// Visual results for CPU
+var cpuAnimal = document.getElementById("cpuAnimal2img");
+var animalImages = ["rat-alive.svg", "pig-alive.svg", "squid-alive.svg"];
+var animalIndex = 0;
+function cpuCycle() {
+    cpuAnimal.setAttribute("src", animalImages[animalIndex]);
+    animalIndex++;
+    if (animalIndex >= animalImages.length) {
+        animalIndex = 0;
+    }
+}
+var intervalHandler = setInterval(cpuCycle, 500);
 
 // Add game logic
 
@@ -17,37 +29,63 @@ function getComputerChoice() {
 
 	return computerChoice;
 }
+// Set globals to grab animal img id
+var rat = document.getElementById("ratButton");
+var pig = document.getElementById("pigButton");
+var squid = document.getElementById("squidButton");
+var results = document.getElementById("resultBox");
 
 function playGame(playerChoice) {
 	var computerChoice = getComputerChoice();
-
+        
 	if (playerChoice === computerChoice) {
-		alert("It's a tie!");
+		cpuAnimal.innerHTML = "Tie!";
+        console.log(playerChoice,computerChoice);
 	}
 
 	if (playerChoice === 'rat') {
+        pig.style.opacity = "0.5";
+        squid.style.opacity = "0.5";
 		if (computerChoice === 'squid') {
-			alert("You Win! Rat nibbles poor squid to death.");
+            clearInterval(intervalHandler);
+			cpuAnimal.setAttribute("src", animalImages[2]);
+            results.innerHTML = "<p style='line-height:100px;'>Win!</p>";
 		} else {
-			alert("You Lose! Rat was squished by piggy.");
+            clearInterval(intervalHandler);
+            cpuAnimal.setAttribute("src", animalImages[1]);
+			results.innerHTML = "<p style='line-height:100px;'>Lose!</p>";
 		}
 	}
 
 	if (playerChoice === 'pig') {
+        rat.style.opacity = "0.5";
+        squid.style.opacity = "0.5";
 		if (computerChoice === 'rat') {
-			alert("You Win! Piggy squishes little rat.");
+            clearInterval(intervalHandler);
+            cpuAnimal.setAttribute("src", animalImages[0]);
+			results.innerHTML = "<p style='line-height:100px;'>Win!</p>";
 		} else {
-			alert("You Lose! Piggy was strangled by squid.");
+            clearInterval(intervalHandler);
+            cpuAnimal.setAttribute("src", animalImages[2]);
+			results.innerHTML = "<p style='line-height:100px;'>Lose!</p>";
 		}
 	}
 
 	if (playerChoice === 'squid') {
+        rat.style.opacity = "0.5";
+        pig.style.opacity = "0.5";
 		if (computerChoice === 'pig') {
-			alert("You Win! Squid strangles litte piggy.");
+			clearInterval(intervalHandler);
+            cpuAnimal.setAttribute("src", animalImages[1]);
+			results.innerHTML = "<p style='line-height:100px;'>Win!</p>";
 		} else {
-			alert("You Lose! Squid was nibbled to death by rat.");
+			clearInterval(intervalHandler);
+            cpuAnimal.setAttribute("src", animalImages[0]);
+			results.innerHTML = "<p style='line-height:100px;'>Lose!</p>";
 		} 
 	}
 
 
 }
+
+// Need a reset button
