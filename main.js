4cb1e41c5b24e1ecf944e0206de1fa71ca17f229
saltymouse <saltymouse@github.com>
2016-04-10 21:42:26 +0900

    add functionality to display winner and loser visually

--- main.js
+++ main.js
@@ -4,17 +4,27 @@ document.getElementById("nojs").style.display = "none";
 // The main game function
 function rps() {
 
-  rpsOptions = {
+  var rpsOptions = {
     // The animals' playing power IS their position in array
     // i.e. rat = 0, pig = 1, squid = 2
     animals: ['rat', 'pig', 'squid'],
     playerChoice: undefined,
-    cpuChoice: undefined
+    cpuChoice: undefined,
+    roundsPlayed: 0
   }
+  
+  reset();
+  
+  //container for the current round number
+  var roundBox = document.getElementById("roundBox");
+  
+  //container element for the text display of the winner
   var resultBox = document.getElementById('resultBox');
-
+  
+  //container element with all the cpu's animals
   var cpuDomDisplay = document.getElementById('cpuBox');
-
+  
+  //container element with all the player's animal choices
   var clickBox = document.getElementById('clickBox');
 
   clickBox.addEventListener('click', getPlayerSelection);
@@ -57,7 +67,7 @@ function rps() {
       victor = 'cpu';
     }
 
-    displayWinner(victor);  
+    displayWinner(victor);
   } // seeWhoWins()
 
   function displayWinner(victor) {
@@ -74,38 +84,70 @@ function rps() {
         document.getElementById(rpsOptions.playerChoice).className = "animated bounceIn"
 
         resultBox.innerHTML = '<p class="animated fadeIn">Draw!</div>';
+        
 
         console.log("Player: "+rpsOptions.playerChoice,"CPU: "+rpsOptions.cpuChoice);
         console.log("draw");
         break;
       case 'player':
-        document.getElementById(rpsOptions.playerChoice).className = "animated bounceIn"
-
+        
+        //set the losing animal image for cpu
+        cpuDomDisplay.children[1].children[0].src = rpsOptions.cpuChoice+"-dead.svg";
+        
+        //set the winning animal image for player
+        clickBox.children[rpsOptions.animals.indexOf(rpsOptions.playerChoice)].children[0].src = rpsOptions.playerChoice+"-alive.svg";
+        
+        //write the winner in the center text-box
         resultBox.innerHTML = '<p class="animated fadeIn">You Win!</div>';
 
+        //console stuff
         console.log("Player: "+rpsOptions.playerChoice,"CPU: "+rpsOptions.cpuChoice);
         console.log("player wins");
         break;
       case 'cpu':
-        document.getElementById(rpsOptions.cpuChoice).className = "animated bounceIn"
+        //set the victorious animal image for cpu
+        cpuDomDisplay.children[1].children[0].src = rpsOptions.cpuChoice+"-alive.svg";
+        
+        //set the losing animal image for player
+        clickBox.children[rpsOptions.animals.indexOf(rpsOptions.playerChoice)].children[0].src = rpsOptions.playerChoice+"-dead.svg";
 
+        //write the winner in the center text-box
         resultBox.innerHTML = '<p class="animated fadeIn">CPU Wins!</div>';
+        
+        //console stuff
         console.log("Player: "+rpsOptions.playerChoice,"CPU: "+rpsOptions.cpuChoice);
         console.log('cpu wins');
         break;
     } // switch (victor)
+    
+    nextRound();
 
-    reset();
   } // displayWinner(victor)
+  
+  function nextRound() {
+    rpsOptions.roundsPlayed += 1;
+    roundBox.innerHTML = "Round "+ rpsOptions.roundsPlayed;
+  }
 
   function reset() {
+    
+    //only allow reset if we've played at least 1 round
+    if (rpsOptions.roundsPlayed >= 1) {
+      
 
-    rpsOptions.playerChoice = undefined;
-    rpsOptions.cpuChoice = undefined;
+      //set the winning animal image for player
+      var playerAnimals = clickBox.children;
+      
+      rpsOptions.animals.forEach(function(animal) {
+        playerAnimals[animal].src = playerAnimals[animal].id + "-alive.svg";
+        console.log(playerAnimals[animal].src)
+      });
 
-    rpsOptions.animals.forEach(function(animal) {
-      document.getElementById(animal).className = "";
-    })
+      //set the victorious animal image for cpu
+      cpuDomDisplay.children[1].children[0].src = rpsOptions.cpuChoice+"-alive.svg";
+      
+      
+    }//endif
 
   }//reset
 
